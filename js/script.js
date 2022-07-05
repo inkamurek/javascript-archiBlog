f445f9f05b02811c2815269c1fbdebe54982320b
inkamurek <zurekkinga@gmail.com>
2022-07-05 14:19:42 +0200

    Add 'active' class

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@
 
 function titleClickHandler(event){
  console.log('Link was clicked!');
+ 
  /* remove class 'active' from all article links */
+ const activeLinks = document.querySelectorAll('.titles a.active');
+for(let activeLink of activeLinks){
+ activeLink.classList.remove('active');
+}
+
  /* add class 'active' to the clicked link */
  /* remove class 'active' from all articles */
  /* get 'href' attribute from the clicked link */
