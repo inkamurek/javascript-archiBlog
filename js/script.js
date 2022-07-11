0b11ed2f9729dbff58d1986a114081ac681f22d8
inkamurek <zurekkinga@gmail.com>
2022-07-11 23:07:21 +0200

    Add in JS activeArticles

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@ for(let activeLink of activeLinks){
 }
 
  /* add class 'active' to the clicked link */
+
+
  /* remove class 'active' from all articles */
+ const activeArticles = document.querySelectorAll('.active');
+for(let activeArticle of activeArticles){
+ activeArticle.classList.remove('active');
+}
+
  /* get 'href' attribute from the clicked link */
+
+
  /* find the correct article using the selector (value of 'href'
 attribute) */
+
+
  /* add class 'active' to the correct article */
+
+
 }
+
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
